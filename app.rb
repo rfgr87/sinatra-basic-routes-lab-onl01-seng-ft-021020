d1702642777604a47d01cdf34b85476996981f6d
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-20 18:31:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get 'name' do 
+  get '/name' do 
     "My name is Roy"
   end 
   
-  get 'hometown' do 
+  get '/hometown' do 
     "My hometown is San Juan"
   end 
   
-  get 'favorite-song' do
+  get '/favorite-song' do
     "My favorite song is Tetras"
   end 
 
